1922efcc51d0e630aa410036711bd885aee5bf12
t3chn0t3s <46801776+t3chn0t3s@users.noreply.github.com>
2024-12-05 03:58:43 -0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,1 @@
-alert("The site is contains a XSS vulnerability and a weak CSP that allows scripts to be loaded from https://*.github.io")
+alert("The site contains a XSS vulnerability and a weak CSP that allows scripts to be loaded from https://*.github.io")
